156484f5973fa9515b0abc4b57a3462ad984817b
sarmistha-jena <sarmistha_jena@apple.com>
2025-10-28 18:25:34 +0530

    fix-2

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,16 +1,19 @@
-def call(String repoUrl, String branch = 'main', String credentialsId = null) {
+// def call(String repoUrl, String branch = 'main', String credentialsId = null) {
+//      def script = this
 
-    // checkout(
-    //     scmGit(
-    //         branches: [[name:  stageParams.branch ]],
-    //         userRemoteConfigs: [[ url: stageParams.url ]]
-    //     )
-    // )
-     def script = this
+//     script.echo "Checking out ${repoUrl} on branch ${branch}..."
 
-    script.echo "Checking out ${repoUrl} on branch ${branch}..."
+//     script.git branch: branch,
+//                  credentialsId: credentialsId,
+//                  url: repoUrl
+// }
 
-    script.git branch: branch,
-                 credentialsId: credentialsId,
-                 url: repoUrl
+def call(Map stageParams) {
+
+    checkout(
+        scmGit(
+            branches: [[name:  stageParams.branch ]],
+            userRemoteConfigs: [[ url: stageParams.url ]]
+        )
+    )
 }
